27b314fd26e94a4ad90b32147d5610390539c06e
VasyaBilak <vasyabilak.vb@gmail.com>
2018-03-18 17:06:43 +0200

    28

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="col-xl-8 col-xs-offset-2">
   <input type="text" class="form-control" [(ngModel)]="searchCar" >
+  <button class="btn btn-primary" (click)="addCar()" >Добавiті</button>
   <hr>
     <ul class="list-group">
       <li 
       class="list-group-item"
-      *ngFor="let car of cars | carFilter:searchCar:'descr'; let i = index"
+      *ngFor="let car of cars | carFilter:searchCar:'name'; let i = index"
       >
       <b>{{i+1}}</b> {{car.name}} <i>{{car.descr}}</i>
       </li>
@@ -26,6 +27,13 @@ export class AppComponent {
      {name: 'Audi', descr: 'WFM 4'},
      {name: 'Mercedes', descr: 'WFM 5'},
      {name: 'BMW', descr: 'WFM 6'}
-   ]
+   ];
+
+   addCar(){
+     this.cars.push({
+       name: 'New Car',
+       descr: 'EFM'
+     });
+   }
 
 }
